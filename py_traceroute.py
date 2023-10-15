dd8b2b29b27da594185d0af4fe578f092ed33d63
Harshit Bangar <harshitbangar@Harshits-MacBook-Pro.local>
2023-10-15 09:07:29 +0530

    more fixes

--- py_traceroute.py
+++ py_traceroute.py
@@ -2,11 +2,13 @@ from scapy.layers.inet import IP, ICMP
 from scapy.sendrecv import sr1
 import sys
 
+MAX_PACKET_HOPS = 64
+
 
 def trace_route(destination_ip):
-    for ttl in range(1, 30):  # Set a maximum TTL value to prevent infinite loops
+    for ttl in range(1, MAX_PACKET_HOPS):  # Set a maximum TTL value to prevent infinite loops
         packet = IP(dst=destination_ip, ttl=ttl) / ICMP()
-        response = sr1(packet, verbose=False, timeout=2)
+        response = sr1(packet, verbose=False, timeout=30)
 
         if response is None:
             print(f"{ttl}: *")
@@ -18,8 +20,8 @@ def trace_route(destination_ip):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 1:
-        print("Usage: cc_trace_route ip_address")
+    if len(sys.argv) != 2:
+        print("Usage: py_traceroute ip_address")
         sys.exit(1)
-    ip_address = sys.argv
+    ip_address = sys.argv[1]
     trace_route(ip_address)
