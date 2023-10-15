e194d965df1c61bce4f798979049c07b7643adff
Harshit Bangar <harshitbangar@Harshits-MacBook-Pro.local>
2023-10-15 08:29:53 +0530

    first commit

--- py_traceroute.py
+++ py_traceroute.py
@@ -1,9 +1,9 @@
 from scapy.layers.inet import IP, ICMP
 from scapy.sendrecv import sr1
+import sys
 
 
 def trace_route(destination_ip):
-
     for ttl in range(1, 30):  # Set a maximum TTL value to prevent infinite loops
         packet = IP(dst=destination_ip, ttl=ttl) / ICMP()
         response = sr1(packet, verbose=False, timeout=2)
@@ -18,4 +18,8 @@ def trace_route(destination_ip):
 
 
 if __name__ == "__main__":
-    trace_route("8.8.8.8")
+    if len(sys.argv) != 1:
+        print("Usage: cc_trace_route ip_address")
+        sys.exit(1)
+    ip_address = sys.argv
+    trace_route(ip_address)
